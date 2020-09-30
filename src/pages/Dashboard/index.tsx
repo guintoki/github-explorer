8d9a0114351c3dfb84f05f1195ea27b679f8a3fb
guintoki <guilherme-vilas-boas@hotmail.com>
2020-09-29 23:39:53 -0300

    add repository style in dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,56 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositiories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />{' '}
+      <Title>Explore repositórios no github</Title>
+      <Form>
+        <input placeholder="Digite o nome do seu repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositiories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/55093125?s=460&u=b241d3376c3c7ddaa21421eb4bcd255962486339&v=4"
+            alt="Guilherme Vilas Boas"
+          />
+          <div>
+            <strong>guintoki/backend</strong>
+            <p>Backend criado para backs</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/55093125?s=460&u=b241d3376c3c7ddaa21421eb4bcd255962486339&v=4"
+            alt="Guilherme Vilas Boas"
+          />
+          <div>
+            <strong>guintoki/backend</strong>
+            <p>Backend criado para backs</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/55093125?s=460&u=b241d3376c3c7ddaa21421eb4bcd255962486339&v=4"
+            alt="Guilherme Vilas Boas"
+          />
+          <div>
+            <strong>guintoki/backend</strong>
+            <p>Backend criado para backs</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositiories>
+    </>
+  );
 };
 
 export default Dashboard;
