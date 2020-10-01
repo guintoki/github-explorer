5bb1e78d0c8a68575c486c6fc4f6565d4cf6d899
guintoki <guilherme-vilas-boas@hotmail.com>
2020-10-01 17:12:07 -0300

    style repository route

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,6 +1,11 @@
 /* eslint-disable no-use-before-define */
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -9,7 +14,41 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository: {params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img src="" alt="profile image" />
+          <div>
+            <strong></strong>
+            <p></p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong></strong>
+            <span></span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link to={`/#`}>
+          <div>
+            <strong>aoba</strong>
+            <p>aoba</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
